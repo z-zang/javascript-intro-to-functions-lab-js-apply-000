a0026ab12fc0ca3a7d623ece3a376aec32a3cb14
Zichao Zang <zang.code@gmail.com>
2018-03-24 16:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,23 +19,22 @@ function sayHiToGrandma(string) {
     return "I love you, too.";
   } else if (string == string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (string !== ){
-    
+  } else if (string != string.toUpperCase()){
+    return "I can't hear you!";
   }
   
 }
 
+// describe('sayHiToGrandma(string)', function() {
+//   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+//     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+//   })
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
+//   it('returns "YES INDEED!" if `string` is uppercase', function() {
+//     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+//   })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+//   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+//     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+//   })
+// })
